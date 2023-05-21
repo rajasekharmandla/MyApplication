39b8e1bdedf9609fbf4bfef14726fa0dc2350fbc
rajasekharmandla <rajasekhar.mandla@gmail.com>
2023-05-21 13:42:31 +0530

    This is the fourth commit

--- src/main/java/org/example/BaseClass.java
+++ src/main/java/org/example/BaseClass.java
@@ -1,33 +1,24 @@
 package org.example;
-
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+
 
 
 public class BaseClass {
-        WebDriver driver;
-        @Before
-        public void setup() {
-            // Set up the wWebDriverManager for chrome driver
-            WebDriverManager.chromedriver().setup();
-            // Create the driver object
-            driver = new ChromeDriver();
-        }
-
-        public void launchApplication(){
-            // Open the browser and go to lambdatest ecommerce website
-            driver.get("https://ecommerce-playground.lambdatest.io/");
-            // Set the current url as a string
-            String url = driver.getCurrentUrl();
-
-        }
-
-        @After
-    public void tearDown(){
-            driver.quit();
-        }
+    public static void main(String[] args){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*","ignore-certificate-errors");
+        WebDriver driver = new ChromeDriver(options);
+        // Set up the wWebDriverManager for chrome driver
+        WebDriverManager.chromedriver().setup();
+        // Open the browser and go to lambdatest ecommerce website
+        driver.get("https://ecommerce-playground.lambdatest.io/");
+        // Set the current url as a string
+        String sTitle = driver.getTitle();
+        Assert.assertTrue(sTitle.equals("Your Store"), "Ecommerce Launched successfully");
+    }
 
 }
